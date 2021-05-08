33a9efbf0f06d7d49ec5e99cd7428bf1e181fb41
Nesterenko <nestslock@gmail.com>
2021-05-08 22:53:10 +0300

    add dataset creating endpoint

--- app/views/dataset.py
+++ app/views/dataset.py
@@ -1,6 +1,13 @@
-from flask import Blueprint, render_template
+import datetime
+import os
+import time
 
+from flask import Blueprint, render_template, current_app, url_for, redirect
+
+from collectors.dataset import DatasetCollector
+from models import db
 from models.appointment import Appointment
+from models.dataset import Dataset, DatasetStatus, DatasetType
 
 bp = Blueprint('dataset', __name__, url_prefix='/dataset')
 
@@ -8,4 +15,40 @@ bp = Blueprint('dataset', __name__, url_prefix='/dataset')
 @bp.route('/', methods=('GET',))
 def main():
     appt_count = Appointment.query.count()
-    return render_template('dataset/main_dataset.html', appt_count=appt_count)
+    datasets = Dataset.query.order_by(Dataset.dt_start).all()
+    return render_template('dataset/main_dataset.html', appt_count=appt_count, datasets=datasets)
+
+
+@bp.route('/create/', methods=('POST',))
+def start_dataset_creating():
+    start = datetime.datetime.now()
+    dataset_name = 'dataset_' + str(time.time()).replace('.', '')
+    dataset_dir = os.path.join(current_app.config['DATASET_DIR'], dataset_name)
+    os.makedirs(dataset_dir, exist_ok=True)
+
+    dataset = Dataset(
+        name=dataset_name,
+        path=dataset_dir,
+        dt_start=start,
+        status=DatasetStatus.start,
+        type=DatasetType.top_one
+    )
+    db.session.add(dataset)
+    db.session.commit()
+
+    collector = DatasetCollector(dataset_dir=dataset_dir)
+
+    try:
+        # TODO: добавить параметры датасета
+        collector.create_doctor_item_base_matrix()
+        collector.create_datasets_for_catboost()
+    except Exception:
+        dataset.status = DatasetStatus.fail
+    else:
+        dataset.status = DatasetStatus.end
+    finally:
+        dataset.dt_end = datetime.datetime.now()
+        db.session.add(dataset)
+        db.session.commit()
+
+    return redirect(url_for('dataset.main'))
